860b198085b1b2ad4a19c9c960e542fa90932392
Syed Ghulam Mustafa <164661872+Syed-Ghulam-Mustafa@users.noreply.github.com>
2025-02-26 23:30:44 +0500

    Add files via upload

---
+++ mad-libs.py
@@ -0,0 +1,14 @@
+#Project 1
+#mad libs game
+#getting inputs from user:
+name = input("Enter a name: ")
+house_name = input("Enter a house name: (e.g. Shah Appartment)")
+surprising_thing = input("Enter a phrase about surprising thing: ")
+
+#creating story:
+print("\n Here is the story:")
+print(f"Once upon a time, there was a person named {name} walking through a society with his friends.")
+print(f"Then he saw a house named {house_name}.")
+print(f"He entered the house and saw a {surprising_thing}.")
+print(f"{name} was surprised and curious to know more about it.")
+print(f"But Then his friends came and told him to get home.")
